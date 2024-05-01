4b331f14c295534579fba4f5f854617b4206ea1b
Giovanna-B-silva <gb688126@gmail.com>
2024-05-01 16:52:41 -0300

    first commit

---
+++ AMST4_Carousel.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AMST4.Carousel.MVC.Context;
+
+namespace AMST4_Carousel.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _DataContext;
+        public CategoryController(DataContext DataContext)
+        {
+            _DataContext = DataContext;
+        }
+        public IActionResult CategoryList()
+        {
+            var categories = _DataContext.Category.ToList();
+            return View(categories);
+        }
+    }
+}
